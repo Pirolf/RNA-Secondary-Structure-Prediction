69dbe78ae3cb90e6f3cad3817afe242e3637f9c7
Yucheng Tu <yuchengtu.uwm@gmail.com>
2014-04-24 03:37:35 -0500

    instance processing draft complete

--- src/RNAPalindrome.java
+++ src/RNAPalindrome.java
@@ -1,9 +1,40 @@
 
 public class RNAPalindrome {
+	//... C           G            G     ...      C        C        G...
+	//....startPos    tmpEndPos   endPos        endMatch          startMatch
+	//HASH: startPos, startMatch
 	private int startPos;
+	private int startMatch;
+	private int tmpEndPos;
+	private int endPos;
+	private int endMatch;
 	private int length;
-	public RNAPalindrome(int startPos, int length){
+	private int seqID;
+	private int hashPalin;
+	public RNAPalindrome(int startPos, int startMatch, int seqID){
 		this.startPos = startPos;
-		this.length = length;
+		this.startMatch = startMatch;
+		this.tmpEndPos = startMatch;
+		this.endPos = 0;
+		this.endMatch = 0;
+		length = 0;
+		this.seqID = seqID;
 	}
+	public void hash(){
+		hashPalin = (startPos + ", " + startMatch).hashCode();
+	}
+	public void setTmpEndPos(int tmp){
+		this.tmpEndPos = tmp;
+	}
+	public int getTmpEndPos(){
+		return this.tmpEndPos;
+	}
+	public void setEnds(int endPos, int endMatch){
+		this.endPos = endPos;
+		this.endMatch = endMatch;
+	}
+	public int getEndPos(){
+		return this.endPos;
+	}
+	
 }
--- src/RNASeqInstance.java
+++ src/RNASeqInstance.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 //import java.sql.*;
 public class RNASeqInstance {
 	public static final char GAP = 'e';
-	
+
 	private int instanceID;
 	private int instanceOrder;
 	private int seqLength;
@@ -16,17 +16,43 @@ public class RNASeqInstance {
 		this.seq = seq;
 		seqMatrix = new int[seqLength + 1][seqLength + 1];
 		String revSeq = reverseSeq(seq);
-		//init matrix: row for originral seq, column for rev
+		//init matrix: row for original seq, column for rev
 		for(int i = 0; i < seqMatrix.length; i++){
 			seqMatrix[i][0] = i;
 			seqMatrix[0][i] = i;
 		}
-		for(int i = 1; i < seqMatrix.length; i++){
-			for(int j = 1; j < seqMatrix.length; j++){
+		/**
+		 * 	A T T C G T A A C C T T G C T A C
+		 * 
+		 * 
+		 * 
+		 */
+		RNAPalindrome currPalin = null;
+		for(int i = 1; i < seqMatrix.length - 4 - 2; i++){//4: no sharp turn, 
+			//length - i - 4 >= 2
+			//length - i - 4 >= j
+			for(int j = 1; j < seqMatrix.length - i - 4; j++){
 				if(seq.charAt(i) == seq.charAt(j)){
 					seqMatrix[i][j] = seqMatrix[i-1][j-1];
 					//TODO: store the info
+					//check 
+					if(currPalin == null){
+						currPalin = new RNAPalindrome(j, seqMatrix.length - i, instanceID);
+					}else{
+						if(currPalin.getEndPos() != 0){
+							//already ended
+							currPalin = new RNAPalindrome(j, seqMatrix.length - i, instanceID);
+						}else{
+							currPalin.setTmpEndPos(j);
+						}				
+					}
 				}else{
+					if(currPalin != null){
+						if(currPalin.getTmpEndPos() == j - 1 && currPalin.getEndPos() == 0){
+							currPalin.setEnds(j, seqMatrix.length - i);
+							palinSeq.add(currPalin);
+						}
+					}
 					if(seqMatrix[i-1][j-1] < seqMatrix[i-1][j] 
 							&& seqMatrix[i-1][j-1] < seqMatrix[i][j-1]){
 						seqMatrix[i][j] = seqMatrix[i-1][j-1] + 1;
@@ -35,14 +61,14 @@ public class RNASeqInstance {
 					}
 				}
 			}
-		
+
 		}
 	}
-	
+
 	public LinkedList<RNAPalindrome> getPalinList(){
 		return palinSeq;
 	}
-	
+
 	/**
 	 * Reverses an RNA seq
 	 * @return
@@ -51,9 +77,17 @@ public class RNASeqInstance {
 		//ATTTCCCGA
 		String reverse = "";
 		for(int i = seq.length() - 1; i >= 0; i--){
-			reverse += seq.charAt(i);
+			char currChar = seq.charAt(i);
+			switch(currChar){
+			case 'A': reverse += 'T';break;
+			case 'T': reverse += 'A';break;
+			case 'G': reverse += 'C';break;
+			case 'C': reverse += 'G';break;
+			default:break;
+			}
+
 		}
 		return reverse;
 	}
-	
+
 }
